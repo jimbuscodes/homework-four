0084b3ca7fbb94baa8d7623f4790a03b4a40d108
jimbusCodes <jimbuscodes@gmail.com>
2025-07-23 13:26:24 -0400

    solved problem 1

--- app/app.js
+++ app/app.js
@@ -1,1 +1,29 @@
 // problem 1
+const calculateGeneration = () => {
+  const age = parseInt(document.getElementById("ageInput").value);
+  const birthYear = new Date().getFullYear() - age;
+  let result = "";
+
+  switch (true) {
+    case birthYear >= 1946 && birthYear <= 1964:
+      result = "You are a member of the Baby Boomer Generation!";
+      break;
+    case birthYear >= 1965 && birthYear <= 1980:
+      result = "You are part of Generation X!";
+      break;
+    case birthYear >= 1981 && birthYear <= 1996:
+      result = "You belong in the Y Generation!";
+      break;
+    case birthYear >= 1997 && birthYear <= 2012:
+      result = "Generation Z is your generation!";
+      break;
+    case birthYear >= 2013 && birthYear <= 2025:
+      result = "You are part of Generation Alpha.";
+      break;
+    default:
+      result = "Sorry - your generation is not listed.";
+      break;
+  }
+
+  document.getElementById("prob1Result").textContent = result;
+};
